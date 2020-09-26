942b682f798f41dacc8dbc69419293d04d4d18b0
epsorokin <epsorokin@gmail.com>
2020-09-26 12:25:05 -0700

    finished assmts

--- Week2/StringsSecondAssignments/Part1.java
+++ Week2/StringsSecondAssignments/Part1.java
@@ -2,7 +2,7 @@
 /**
  * Week 2: Find a gene with any of three stop codons
  * @author EPS
- * @version 2020-09-13
+ * @version 2020-09-25
  */
 
 import edu.duke.StorageResource;
@@ -20,67 +20,91 @@ public class Part1 {
                 return currIndex;
             } 
             else {
-                //If not: Update currIndex
+                //If not: Update currIndex to be next index of a stop codon 
                 currIndex = dna.indexOf(stopCodon, currIndex + 1); 
             }
              
         }
         // If there is no such stopCodon, this method returns the length of the dna strand.
-        return dna.length(); 
+        //return dna.length(); 
+        return -1; 
     }
-    //method to test findStopCodon()
-    public void testFindStopCodon() {
-        String dna1 = "ATGTGGCGCCAATAATCT";
-        System.out.println("DNA Strand is ;" + dna1);
-        int startIndex = 0;
-        String stopCodon = "TAA";
-        String stopIndex = findStopCodon(dna1, startIndex, stopCodon);
-        //System.out.println("Stop index is : ", (char) stopIndex); 
-    }
-    
-    public String findGene(String dna) {
-        //Find first occurrence of "ATG" and call its index startIndex
-        int startIndex = dna.indexOf("ATG");
+
+    public String findGene (String dna, int where) {
+        //Find the index of the first occurrence of the start codon "ATG" or return empty string
+        int startIndex = dna.indexOf("ATG", where);
         if (startIndex == -1) {
             return "";
         }
-        
-        //Find the "TAA", "TAG","TGA" starting from (startIndex+3), call this result currIndex
-        int taaIndex = findStopCodon(dna, startIndex, "TAA");
-        int tagIndex = findStopCodon(dna, startIndex, "TAG");
-        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
-        
-        //Initialize minIndex
-        int minIndex = 0; 
-        //Logic to update minIndex 
-        if (taaIndex == -1  || 
-            (tgaIndex != -1 && tgaIndex < taaIndex)){
-                minIndex = tgaIndex;
-            }
-        else {
-                minIndex = taaIndex;
-            }
-        if (minIndex == -1 ||
-            (tagIndex != -1 && tagIndex < minIndex)){
-                minIndex = tagIndex;
-            }
-        
-        if (minIndex == -1 ) {
-            return ""; 
-            }
-                
-        if (minIndex == dna.length()) {
+        int tagIndex = findStopCodon(dna, startIndex + 3, "TAG");
+        if (tagIndex == -1) {
             return "";
-        }
-        return dna.substring(startIndex, minIndex + 3);
+        } 
+        return dna.substring(startIndex, tagIndex + 3); 
+        
     }
-
-   
-        public void testOn(String dna ) {
-        //ATG TAA ATG TGA 
-        testOn("ATGATCTAATTTATGCTGCAACGGTGAAGA");
-        testOn("");
-        // ATG.... ATGTAA 
-        testOn("ATGATCATAAGAAGAATAATAAGAGGGGCCCCATGTAA");
+    public void printAllGenes(String dna) {
+        //Find genes until there are no more 
+        //Initialize start index
+        int startIndex = 0;
+        while (true) {
+            String currGene = findGene(dna, startIndex);
+            //if no gene
+            if (currGene.isEmpty()) {
+                break;
+            }
+            //Print that gene out 
+            System.out.println(currGene);
+            //Set startIndex to just past the end of the gene
+            startIndex = dna.indexOf(currGene, startIndex) + currGene.length();
+        }
+    }
+    public void testFindStopCodon() {
+        //4. Call the method with several examples 
+        System.out.println("");
+        System.out.println("testFindStopCodon...");
+        //Example with TAA stop codon
+        String dna1 = "ATGCCACGCTAA";
+        System.out.println("The DNA Strand is:   " + dna1);
+        int stopIndex1 = findStopCodon(dna1, 0, "TAA"); 
+        System.out.println("The stop index is :   " + stopIndex1);
+        //Example with no stop codon
+        String dna2 = "ATGGCTCTTAGGATGG";
+        System.out.println("The DNA Strand is:   " + dna2);
+        
+        int stopIndex2 = findStopCodon(dna2, 0, "TAG");
+        System.out.println("The stop index is :   " + stopIndex2);
+    }
+    public void testFindGene() {
+        //no ATG
+        System.out.println("");
+        System.out.println("Test findGene...");
+        String dna1 = "CGCTAACTAGCT";
+        String gene1 = findGene(dna1,0);
+        System.out.println("The DNA Strand is: " + dna1);
+        System.out.println("The gene is: " + gene1);
+        //ATG and one valid stop codon
+        String dna2 = "ATGCAGCGGTCTTAG";
+        String gene2 = findGene(dna2,0);
+        System.out.println("The DNA Strand is: " + dna2);
+        System.out.println("The gene is: " + gene2);
+        //ATG and multiple valid stop codons
+        String dna3 = "TAGATGCGTGACTAGCGTTAG";
+        String gene3 = findGene(dna3,0);
+        System.out.println("The DNA Strand is: " + dna3);
+        System.out.println("The gene is: " + gene3);
+        //ATG with no valid stop codons
+        String dna4 = "GGATGCGATTCGATATTA";
+        System.out.println("The DNA Strand is: " + dna4);
+        String gene4 = findGene(dna4,0);
+        System.out.println("The gene is: " + gene4);
+        
+    }
+    public void testOn() {
+        System.out.println("");
+        System.out.println("Test testFindAllGenes...");
+        String dna1 = "AATGCGCTAGCTAGCTATGCGACTAATCATGTAGAAA";
+        findAllGenes(dna1);
+        
     }
 }
