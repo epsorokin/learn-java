d9cc516d09fc7b029e82747a1fc88be0d08bf201
epsorokin <epsorokin@gmail.com>
2020-09-26 12:30:32 -0700

    initial commit of part2

--- Week2/StringsSecondAssignments/Part1.java
+++ Week2/StringsSecondAssignments/Part1.java
@@ -9,44 +9,59 @@ import edu.duke.StorageResource;
 
 public class Part1 {
     public int findStopCodon (String dna, int startIndex, String stopCodon) {
+        
         //Find "TAA" or other stop codn starting from (startIndex + 3), call this result currIndex
         //startIndex = dna.indexOf("ATG");
         int currIndex = dna.indexOf(stopCodon, startIndex + 3);
+        
         while (currIndex != -1) {
+            
             //If (currIndex - startIndex) is mulitple of 3
             int diff = currIndex - startIndex;
+            
             if ((diff % 3) == 0) {
                 //If so: Answer 
                 return currIndex;
             } 
+            
             else {
                 //If not: Update currIndex to be next index of a stop codon 
                 currIndex = dna.indexOf(stopCodon, currIndex + 1); 
             }
              
         }
+        
+        
         // If there is no such stopCodon, this method returns the length of the dna strand.
         //return dna.length(); 
         return -1; 
     }
 
     public String findGene (String dna, int where) {
+        
         //Find the index of the first occurrence of the start codon "ATG" or return empty string
         int startIndex = dna.indexOf("ATG", where);
+        
         if (startIndex == -1) {
             return "";
         }
+        
         int tagIndex = findStopCodon(dna, startIndex + 3, "TAG");
+        
         if (tagIndex == -1) {
             return "";
         } 
+        
         return dna.substring(startIndex, tagIndex + 3); 
         
     }
     public void printAllGenes(String dna) {
+        
         //Find genes until there are no more 
         //Initialize start index
+        
         int startIndex = 0;
+        
         while (true) {
             String currGene = findGene(dna, startIndex);
             //if no gene
---
+++ Week2/StringsSecondAssignments/Part2.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of Part2 here.
+ * 
+ * @author (EPS) 
+ * @version (2020-09-26)
+ */
+public class Part2 {
+    //HowMany method takes two string paramters
+    public String howMany(String a, String b) {
+    //Return integer indicating how many times stirng a appears in stringb where each occurrence must not overlap
+    
+    return int
+    }
+    
+}
