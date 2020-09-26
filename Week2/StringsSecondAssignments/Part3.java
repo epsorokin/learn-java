75300635e219dee3779c2c524992a23ed64d1adc
epsorokin <epsorokin@gmail.com>
2020-09-26 13:00:17 -0700

    completed part3

--- Week2/StringsSecondAssignments/Part3.java
+++ Week2/StringsSecondAssignments/Part3.java
@@ -29,7 +29,6 @@ public class Part3 {
              
         }
         
-        
         // If there is no such stopCodon, this method returns the length of the dna strand.
         //return dna.length(); 
         return -1; 
@@ -61,15 +60,49 @@ public class Part3 {
         int startIndex = 0;
         
         while (true) {
+            
             String currGene = findGene(dna, startIndex);
+            
             //if no gene
             if (currGene.isEmpty()) {
                 break;
             }
+            
             //Print that gene out 
             System.out.println(currGene);
+            
             //Set startIndex to just past the end of the gene
             startIndex = dna.indexOf(currGene, startIndex) + currGene.length();
         }
+    }
+    public int countGenes (String dna) {
+        
+        //Return the number of genes found in the dna 
+        int startIndex = 0;
+        int geneCount = 0;
+        
+        while (true) {
+            
+            String currGene = findGene(dna, startIndex);
+            if (currGene.isEmpty()) {
+                break;
+            }
+            
+            geneCount = geneCount + 1;
+            startIndex = dna.indexOf(currGene,startIndex) + currGene.length();
+        }
+        
+        return geneCount;
+    
+    }
+    public void testCountGenes() {
+        //Initial example for testing
+        System.out.println("");
+        System.out.println("Test testCountGenes...");
+        String dna1 = "AATGCGCTAGCTAGCTATGCGACTAATCATGTAGAAA";
+        System.out.println("DNA Strand is: " + dna1);
+        int geneCount1 = countGenes(dna1);
+        System.out.println("Gene count is : " + geneCount1);
+        
     }
 }
