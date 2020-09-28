5f3a187beaa3fa1182e531c05fd7025d6d192c3f
epsorokin <epsorokin@gmail.com>
2020-09-27 21:29:54 -0700

    initial commit

---
+++ Week3/parsingExportData/Part1.java
@@ -0,0 +1,20 @@
+package parsingExportData;
+
+
+/**
+ * Programming Exercise: Parsing Export Data
+ * 
+ * @author (EPS) 
+ * @version (2020-09-27)
+ */
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.*;
+
+public class Part1 {
+    public void tester() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+    }
+}
